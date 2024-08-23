242b69b969267e5188f8a8e322a8daec0d95017d
Liliya <Kilechkaheh@yandex.ru>
2024-08-24 01:10:05 +0300

    2 проверка

--- stats-server/service/src/main/java/ru/yandex/practicum/exceptions/ErrorHandler.java
+++ stats-server/service/src/main/java/ru/yandex/practicum/exceptions/ErrorHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.format.DateTimeParseException;
+
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
@@ -15,5 +17,12 @@ public class ErrorHandler {
     public ErrorResponse handlerAnyException(final Exception e) {
         log.error("Error", e);
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerDateTimeParseException(final DateTimeParseException e) {
+        log.error("Error", e);
+        return new ErrorResponse("Неверный формат даты");
     }
 }
