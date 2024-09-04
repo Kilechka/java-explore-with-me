032826d0c8b1b5f667c25b1deac09439c828159e
Liliya <Kilechkaheh@yandex.ru>
2024-09-05 01:56:43 +0300

    fix codeStyle

--- stats-server/service/src/main/java/ru/yandex/practicum/ServiceStatImpl.java
+++ stats-server/service/src/main/java/ru/yandex/practicum/ServiceStatImpl.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum;
 
-import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
